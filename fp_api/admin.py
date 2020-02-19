2fbda3c1ec4c4351d1650188c2789e6e20832168
jehyunJung <toojey7@gmail.com>
2020-02-19 09:09:34 +0000

    feat: add program synchronizer

--- fp_api/admin.py
+++ fp_api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-model_list=[Cpu,Gpu,Laptop,Game]
+model_list=[Cpu,Gpu,Laptop,Game,Program]
 
 for model in model_list:
     admin.site.register(model)
