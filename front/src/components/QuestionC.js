355ac5116c273b0b9078b396aa94b1e7a597e222
jehyunJung <toojey7@gmail.com>
2020-02-20 02:53:06 +0000

    ui: request program data from db

--- front/src/components/QuestionC.js
+++ front/src/components/QuestionC.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import server from '../config'
+import axios from 'axios'
 import {
     FormGroup,
     FormControl,
@@ -7,58 +9,97 @@ import {
     Checkbox,
     Button
 } from '@material-ui/core'
-class QuestionC extends React.Component {
+
+
+
+class QuestionA extends React.Component {
     state = {
-        photoshop: false,
-        premiere_pro: false,
-        auto_cad: false,
-        vmware: false
+        programs: {}
     }
+
+    FormControls = []
+
     handlePrevious = () => {
         this.props.goPreviousSurvey();
     }
-    handleNext = () => {
-        this.props.setParentAnswer("programs",this.state);
-        this.props.goNextSurvey();
+    handleNext = function () {
+        this.props.setParentAnswer("programs", this.state.programs)
+        this.props.goNextSurvey()
     }
-    handleChange = (name) => {
-        this.setState({[name]: !this.state[name]})
+
+    handleChange = function (name) {
+        this.setState(current => {
+            let newPrograms = current.programs;
+            newPrograms = { ...newPrograms, [name]: !(current.programs[name]) }
+            return {
+                programs: newPrograms
+            }
+        })
     }
-    
+
+    getPrograms = async () => {
+        const BASEURL = server.getBASEURL();
+        let data;
+        try {
+            const res = await axios.get(`${BASEURL}/fp_api/programs/`)
+            data = res.data
+        }
+        catch (Error) {
+            console.log(Error)
+        }
+        return data
+    }
+
+    //setState는 절대로 반복문 안에서 호출하지 않는다.
+    loadPrograms = (data) => {
+        let newPrograms = {}
+        for (let idx in data) {
+            newPrograms[data[idx].name] = false;
+        }
+        this.setState({
+            programs: newPrograms
+        })
+    }
+
+    renderForms = (programs) => {
+        let FormControls = []
+        for (let programName in programs) {
+            FormControls.push(
+                <FormControlLabel
+                        key = {programName}
+                        control={<Checkbox
+                        checked={programs.programName}
+                        onChange={() => this.handleChange(programName)}
+                    ></Checkbox>}
+                    label={programName}
+                />
+            )
+        }
+        return FormControls
+    }
+
+    // Data 구독은 did mount에서
+    async componentDidMount() {
+        const data = await this.getPrograms()
+        this.loadPrograms(data)
+    }
+
     render() {
         return (
             <div id="QuestionC">
                 <FormControl>
-                    <FormLabel component="legend">Check all the programs you use</FormLabel>
-                    {/* demo */ }
+                    <FormLabel component="legend">Check all the games you play</FormLabel>
                     <FormGroup>
-                        <FormControlLabel
-                            control={ <Checkbox checked={ this.state.photoshop } onChange={ () =>this.handleChange('photoshop') }  ></Checkbox> }
-                            value="photoshop"
-                            label="Adobe Photoshop"
-                        />
-                        <FormControlLabel
-                            control={ <Checkbox checked={ this.state.premiere_pro } onChange={ ()=>this.handleChange('premiere_pro') }  ></Checkbox> }
-                            value="premiere_pro"
-                            label="Adobe Premiere Pro" />
-                        <FormControlLabel
-                            control={ <Checkbox checked={ this.state.auto_cad } onChange={ ()=>this.handleChange('auto_cad') }  ></Checkbox> }
-                            value="auto_cad"
-                            label="IBM Auto_cad" />
-                        <FormControlLabel
-                            control={ <Checkbox checked={ this.state.vmware } onChange={ ()=>this.handleChange('vmware') }  ></Checkbox> }
-                            value="vmware"
-                            label="Vmware playerstation" />
+                        {this.renderForms(this.state.programs)}
                     </FormGroup>
-                    {/* demo end */ }
                 </FormControl>
                 <div>
-                    <Button onClick={ () => { this.handlePrevious() } } variant='contained'>이전</Button>
-                    <Button onClick={ () => { this.handleNext() } } variant='contained'>다음</Button>
+                    <Button onClick={() => { this.handlePrevious() }} variant='contained'>이전</Button>
+                    <Button onClick={() => { this.handleNext() }} variant='contained'>다음</Button>
                 </div>
             </div>
         );
     }
 }
 
-export default QuestionC
+export default QuestionA
