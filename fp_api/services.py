092f9d5c6acbdbeca22eb17f1b111f3c905a4a41
jehyunJung <toojey7@gmail.com>
2020-02-20 08:46:02 +0000

    feat: recommend url mapping

---
+++ fp_api/services.py
@@ -0,0 +1,25 @@
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse,HttpResponse
+from .serializers import *
+import json
+
+
+@csrf_exempt
+def recommend(request):
+    if request.method =="POST":
+        print(request.body)
+        body=json.loads(request.body.decode('utf-8'))
+        print(body)
+        print('GAME: ',body['games'])
+        print('OUTDOOR: ',body['outdoor'])
+        print('BAG: ',body['bag'])
+        print('PROGRAMS: ',body['programs'])
+        print('BUDGET: ',body['budget'])
+        print('PRIORITY: ',body['priority'])
+        queryset=Laptop.objects.filter(ram__gte=16)
+        #queryset=Program.objects.get(name__startswith='unreal').min_cpu_set.all()
+        serializer=LaptopSerializer(queryset,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    else:
+        print('LOG:GET')
--- fp_api/urls.py
+++ fp_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from fp_api import views
 from rest_framework import routers
+from . import services
 
 router = routers.DefaultRouter()
 router.register('cpus',views.CpuViewSet)
@@ -10,6 +11,7 @@ router.register('games',views.GameViewSet)
 router.register('programs',views.ProgramViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('services/recommend/',services.recommend)
     # path('asdfasdf/', views.asdfasdf, exampleparameter='wfeefsdf')
 ]
